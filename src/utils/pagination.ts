df94c8c6f4b073569713fa66542830075e944bec
Bryan José Mañon Colón <Bryann1820@gmail.com>
2023-10-11 20:52:19 -0400

    first commit

---
+++ src/utils/pagination.ts
@@ -0,0 +1,31 @@
+const isNumeric = (str: string) => /^\d+$/.test(str);
+import { allPosts } from "contentlayer/generated";
+
+const items = allPosts.sort((a, b) => b.date.localeCompare(a.date));
+
+
+export const getPagination = <T>(
+  items: T[],
+  itemsPerPage = 2,
+  currentPage = "1"
+) => {
+  if (!isNumeric(currentPage)) {
+    throw new Error("Not a number");
+  }
+
+  const currentPageInt = parseInt(currentPage);
+  const totalItems = items.length;
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  if (currentPageInt > totalPages) {
+    throw new Error(`Page ${currentPageInt} does not exist`);
+  }
+
+  const offset = (currentPageInt - 1) * itemsPerPage;
+  const currentItems = items.slice(offset, offset + itemsPerPage);
+
+  return {
+    currentItems,
+    totalPages,
+  };
+};
